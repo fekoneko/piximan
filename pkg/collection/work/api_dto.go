a6bc89881d8ab721f5c8c11281821e19b1eb027d
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-03-25 01:21:35 +0300

    fix: make series data nullable

--- pkg/collection/work/api_dto.go
+++ pkg/collection/work/api_dto.go
@@ -21,10 +21,10 @@ type ApiDto struct {
 	LikeCount     uint64 `json:"likeCount"`
 	CommentCount  uint64 `json:"commentCount"`
 	UploadDate    string `json:"uploadDate"`
-	SeriesNavData *struct {
-		SeriesId string `json:"seriesId"`
-		Order    uint64 `json:"order"`
-		Title    string `json:"title"`
+	SeriesNavData struct {
+		SeriesId *string `json:"seriesId"`
+		Order    *uint64 `json:"order"`
+		Title    *string `json:"title"`
 	} `json:"seriesNavData"`
 	Tags struct {
 		Tags [](struct {
@@ -38,13 +38,10 @@ func (dto *ApiDto) Work(downloadTime time.Time) *Work {
 	userId, _ := strconv.ParseUint(dto.UserId, 10, 64)
 	uploadTime, _ := time.Parse(dto.UploadDate, dto.UploadDate) // FIXME: parsing is broken
 
-	var seriesId uint64
-	var seriesTitle string
-	var seriesOrder uint64
-	if dto.SeriesNavData != nil {
-		seriesId, _ = strconv.ParseUint(dto.SeriesNavData.SeriesId, 10, 64)
-		seriesTitle = dto.SeriesNavData.Title
-		seriesOrder = dto.SeriesNavData.Order
+	var seriesId *uint64
+	if dto.SeriesNavData.SeriesId != nil {
+		id, _ := strconv.ParseUint(*dto.SeriesNavData.SeriesId, 10, 64)
+		seriesId = &id
 	}
 
 	tags := make([]string, len(dto.Tags.Tags))
@@ -69,9 +66,9 @@ func (dto *ApiDto) Work(downloadTime time.Time) *Work {
 		CommentCount:  dto.CommentCount,
 		UploadTime:    uploadTime,
 		DownloadTime:  downloadTime,
-		SeriesId:      &seriesId,
-		SeriesTitle:   &seriesTitle,
-		SeriesOrder:   &seriesOrder,
+		SeriesId:      seriesId,
+		SeriesTitle:   dto.SeriesNavData.Title,
+		SeriesOrder:   dto.SeriesNavData.Order,
 		Tags:          tags,
 	}
 }
