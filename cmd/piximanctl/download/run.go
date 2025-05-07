8cc1f4d9bb9fd1b19b35628705f2691896d9c4b6
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-05-07 03:37:52 +0300

    doc: add todos and help for bookmarks download

--- cmd/piximanctl/download/run.go
+++ cmd/piximanctl/download/run.go
@@ -4,12 +4,12 @@ import "os"
 
 type options struct {
 	Ids         *[]uint64 `short:"i" long:"id"`
-	Kind        *string   `short:"t" long:"type"`
-	Size        *uint     `short:"s" long:"size"`
-	Path        *string   `short:"p" long:"path"` // TODO: Paths array
 	QueuePath   *string   `short:"l" long:"list"`
 	InferIdPath *string   `short:"I" long:"inferid"`
+	Kind        *string   `short:"t" long:"type"`
+	Size        *uint     `short:"s" long:"size"`
 	OnlyMeta    *bool     `short:"m" long:"onlymeta"`
+	Path        *string   `short:"p" long:"path"`
 	Password    *string   `short:"P" long:"password"`
 }
 
--- cmd/piximanctl/help/run_download.go
+++ cmd/piximanctl/help/run_download.go
@@ -6,14 +6,21 @@ const DOWNLOAD_HELP = //
 `Run without arguments to enter interactive mode.
 
 > piximanctl download [--id        ...] [--type ...] [--path     ...]
-                      [--list      ...] [--size ...] [--password ...]
-                      [--inferid   ...] [--onlymeta]
+                      [--bookmarks ...] [--size ...] [--password ...]
+                      [--list      ...] [--onlymeta]
+                      [--inferid   ...]
 
                               Download sources
                               ----------------
 --id         ID of the downloaded work. You can found it in the work URI:
  -i          https://www.pixiv.net/artworks/12345 <- 12345 is the ID here.
              Can be provided multiple times.
+
+--bookmarks  Download bookmarks of the user. You can provide one of these values:
+ -b          - <user ID>    : download public bookmarks of the specified user
+             - my           : download all bookmarks of the authorized user if
+                              session ID is available
+             ! unimplemented !
 
 --list       Path to a file with information about which works to download.
  -l          The file must contain a list in YAML format, for example:
@@ -69,8 +76,11 @@ const DOWNLOAD_HELP = //
 # Download artwork with ID 10000 to your possible collection directory
 > piximanctl download --id 10000 --path "$HOME/My Collection/{userid}/{id}"
 
-# Download in slamm size and pass password from X11 clipboard to authorize
-> piximanctl download --id 10000 --size 1 --password $(xclip -o)
+# Download own bookmarks in small size and pass password from X11 clipboard
+> piximanctl download --bookmarks my --size 1 --password $(xclip -o)
+
+# Download public bookmarks of the user with ID 10000
+> piximanctl download --bookmarks 10000
 
 # Download novel with ID 10000 to the current directory
 > piximanctl download --id 10000 --type novel
@@ -85,3 +95,9 @@ const DOWNLOAD_HELP = //
 func RunDownload() {
 	fmt.Print(DOWNLOAD_HELP)
 }
+
+// TODO: public user bookmarks download + remove unimplemented message in the help
+// TODO: authorized user bookmarks download (+private) + remove unimplemented message in the help
+// TODO: download bookmarks of type (novel, artwork) + example in the help
+// TODO: bookmarks --from, --to (offset and limit) + white about it in the help + example
+// TODO: bookmarks --newer, --older than date + white about it in the help + example
