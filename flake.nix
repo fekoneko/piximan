429898680373ca734b45e6795a0276449034dba6
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-06-22 02:51:13 +0300

    generate version

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
       src = self;
       vendorHash = "sha256-2aoU7xoumrq+0rQ0aIHoVLTWpJka8Q3XWuELkKAO4fc=";
       proxyVendor = false;
+      version = builtins.readFile ./.version;
 
       allSystems = [
         "x86_64-linux" # 64-bit Intel/AMD Linux
@@ -25,7 +26,7 @@
     {
       packages = forAllSystems ({ pkgs }: {
         default = pkgs.buildGoModule {
-          inherit src vendorHash;
+          inherit src vendorHash version;
           name = "piximan";
 
           buildInputs = with pkgs; [
@@ -42,7 +43,7 @@
             blueprint-compiler
           ];
 
-          # TODO: ldflags = [ "-X main.version=${version}" ];
+          ldflags = [ "-X main.version=${version}" ];
           preBuild = "bash ./compile-resources.sh";
         };
       }
---
+++ generate-version.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git describe --always --tags > .version
