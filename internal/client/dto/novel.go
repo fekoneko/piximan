822cf65a245d00b7b56c406d5a4e4ea8f4c7513f
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-07-12 15:34:37 +0300

    parse all novel content patterns

--- internal/client/dto/novel.go
+++ internal/client/dto/novel.go
@@ -1,8 +1,7 @@
 package dto
 
 import (
-	"fmt"
-	"strconv"
+	"regexp"
 	"strings"
 	"time"
 
@@ -26,84 +25,120 @@ type Novel struct {
 
 func (dto *Novel) FromDto(downloadTime time.Time) (w *work.Work, pages *[]string, coverUrl *string) {
 	w = dto.Work.FromDto(utils.ToPtr(work.KindNovel), downloadTime)
-
-	pages, _ = parseContent(w, dto.Content)
+	pages = parseContent(dto.Content)
 
 	return w, pages, dto.CoverUrl
 }
 
-// TODO: parse page breaks (multiple pages)
-// TODO: parse page titles
-// TODO: parse page links
-// TODO: parse url links
-// TODO: parse ruby (furigana)
-// TODO: parse pixiv images
-// TODO: ensure image extensions are correct when quolity is 'original'
-
 // Convert novel content from pixiv format to markdown. This does the following:
-// - \n -> \n\n
-// - [uploadedimage:{id}] -> ![Embedded Image](./{id}. {title}.{ext})
-// - [pixivimage:{id}] -> ![Embedded Image](./{id}. {title}.{ext})
+// - 2 or more \n -> \n\n
+// - \n -> <br>
+// - [newpage] -> write to next page and trim empty lines at the beginning and at the end
+// - [uploadedimage:{id}] -> ![Illustration](./{id}. {title}.{ext})
+// - [pixivimage:{id}] -> ![Illustration](./{id}. {title}.{ext})
 // - [[rb:{word} > {ruby}]] -> <ruby>{word}<rt>{ruby}</rt></ruby>
-// - [newpage]\n -> write to next page file
 // - [chapter:{title}] -> # {title}
 // - [jump:{page}] -> [{page}](./{page}. {title}.md)
 // - [[jumpuri:{title} > {url}]] -> [{title}]({url})
-// Also returns a map to later translate Pixiv image ids to local IDs.
-func parseContent(w *work.Work, content *string) (pages *[]string, imageMap map[uint64]uint64) {
-	imageMap = make(map[uint64]uint64)
-	charsOnLine := 0
-	latestImageId := 1
-	builder := strings.Builder{}
-
+func parseContent(content *string) (pages *[]string) {
 	if content == nil {
-		return nil, imageMap
+		return nil
 	}
 
-	for i := 0; i < len(*content); i++ {
-		if (*content)[i] == '\n' {
+	pages = utils.ToPtr(make([]string, 0, 1))
+	matches := contentRegexp.FindAllStringSubmatchIndex(*content, -1)
+	builder := strings.Builder{}
+	prevEnd := 0
+
+	for _, match := range matches {
+		if prevEnd < match[0] {
+			builder.WriteString((*content)[prevEnd:match[0]])
+		}
+		prevEnd = match[1]
+
+		if match[newPage] >= 0 {
+			builder.WriteString("\n")
+			*pages = append(*pages, builder.String())
+			builder.Reset()
+		} else if match[startNewLines] >= 0 {
+		} else if match[endNewLines] >= 0 {
+		} else if match[newParagraph] >= 0 {
 			builder.WriteString("\n\n")
-			charsOnLine = 0
-		} else if id := embeddedImage(content, &i); id != 0 {
-			latestImageId++
-			title := utils.FromPtr(w.Title, "unknown")
-			s := fmt.Sprintf("![Embedded Image](./%03d. %v.jpg)", latestImageId, title)
-			builder.WriteString(s)
-		} else {
-			builder.WriteByte((*content)[i])
-			charsOnLine++
+		} else if match[newLine] >= 0 {
+			builder.WriteString("<br>")
+		} else if match[uploadedImage] >= 0 {
+			builder.WriteString("![Illustration]()") // TODO: filename
+		} else if match[pixivImage] >= 0 {
+			builder.WriteString("![Illustration]()") // TODO: filename
+		} else if match[ruby] >= 0 {
+			word := (*content)[match[rubyWord]:match[rubyWord+1]]
+			ruby := (*content)[match[rubyRuby]:match[rubyRuby+1]]
+			builder.WriteString("<ruby>")
+			builder.WriteString(word)
+			builder.WriteString("<rt>")
+			builder.WriteString(ruby)
+			builder.WriteString("</rt></ruby>")
+		} else if match[title] >= 0 {
+			text := (*content)[match[titleText]:match[titleText+1]]
+			builder.WriteString("# ")
+			builder.WriteString(text)
+		} else if match[pageLink] >= 0 {
+			page := (*content)[match[pageLinkPage]:match[pageLinkPage+1]]
+			builder.WriteByte('[')
+			builder.WriteString(page)
+			builder.WriteString("]()") // TODO: filename
+		} else if match[urlLink] >= 0 {
+			text := (*content)[match[urlLinkText]:match[urlLinkText+1]]
+			url := (*content)[match[urlLinkUrl]:match[urlLinkUrl+1]]
+			builder.WriteByte('[')
+			builder.WriteString(text)
+			builder.WriteString("](")
+			builder.WriteString(url)
+			builder.WriteByte(')')
 		}
 	}
 
-	parsed := builder.String()
-	return &[]string{parsed}, imageMap
+	if prevEnd < len(*content) {
+		builder.WriteString((*content)[prevEnd:len(*content)])
+	}
+	builder.WriteString("\n")
+	*pages = append(*pages, builder.String())
+
+	return pages
 }
 
-// Determine if embedded image annotation starts at index i. If it is, return the image ID,
-// otherwise return 0. Content is assumed to be not nil and have a length of at least i+1.
-// The func tion advances i to the last character of found annotation.
-func embeddedImage(content *string, i *int) uint64 {
-	const annotation = "uploadedimage:"
+var contentRegexp = regexp.MustCompile(
+	`(\n*\[newpage\]\n*)|` +
+		`(^\n*)|` +
+		`(\n*$)|` +
+		`(\n{2,})|` +
+		`(\n)|` +
+		`(\[uploadedimage:([0-9]+)\])|` +
+		`(\[pixivimage:([0-9]+)\])|` +
+		`(\[\[rb:(.+) *> *(.+)\]\])|` +
+		`(\[chapter:(.+)\])|` +
+		`(\[jump:([0-9]+)\])|` +
+		`(\[\[jumpuri:(.+) *> *(.+)\]\])`,
+)
 
-	if (*content)[*i] != '[' ||
-		len(*content) <= *i+len(annotation)+3 ||
-		(*content)[*i+1:*i+len(annotation)+1] != annotation {
-		return 0
-	}
-
-	idStart := *i + len(annotation) + 1
-	idLength := strings.IndexByte((*content)[idStart:], ']')
-	if idLength == -1 {
-		return 0
-	}
-
-	idEnd := idStart + idLength
-	id := (*content)[idStart:idEnd]
-	parsed, err := strconv.ParseUint(id, 10, 64)
-	if err != nil {
-		return 0
-	}
-
-	*i = idEnd
-	return parsed
-}
+const (
+	newPage         = 2 * 1
+	startNewLines   = 2 * 2
+	endNewLines     = 2 * 3
+	newParagraph    = 2 * 4
+	newLine         = 2 * 5
+	uploadedImage   = 2 * 6
+	uploadedImageId = 2 * 7
+	pixivImage      = 2 * 8
+	pixivImageId    = 2 * 9
+	ruby            = 2 * 10
+	rubyWord        = 2 * 11
+	rubyRuby        = 2 * 12
+	title           = 2 * 13
+	titleText       = 2 * 14
+	pageLink        = 2 * 15
+	pageLinkPage    = 2 * 16
+	urlLink         = 2 * 17
+	urlLinkText     = 2 * 18
+	urlLinkUrl      = 2 * 19
+)
