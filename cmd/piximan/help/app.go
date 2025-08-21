331d7ce65e20f1bcdf00a1af05c7ecbc1b8c05f7
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-08-21 12:57:59 +0300

    rename `APP_HELP` constant to be in appropriateCase

--- cmd/piximan/help/app.go
+++ cmd/piximan/help/app.go
@@ -2,11 +2,11 @@ package help
 
 import "fmt"
 
-const APP_HELP = //
+const appHelp = //
 `> piximan      # Run piximan collection viewer
 > piximan app  # Allows to provide arquments to the viewer
 `
 
 func RunApp() {
-	fmt.Print(APP_HELP)
+	fmt.Print(appHelp)
 }
