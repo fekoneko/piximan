82b218ef6d4560590e4c36ef54f35fb8ead8e006
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-09-06 13:22:15 +0300

    feat: Add `language` field to download lists

--- internal/fsext/dto/list.go
+++ internal/fsext/dto/list.go
@@ -13,6 +13,7 @@ type List []struct {
 	Id       *uint64   `yaml:"id,omitempty"`
 	Kind     *string   `yaml:"type,omitempty"`
 	Size     *uint64   `yaml:"size,omitempty"`
+	Language *string   `yaml:"language,omitempty"`
 	OnlyMeta *bool     `yaml:"only-meta,omitempty"`
 	Paths    *[]string `yaml:"paths,omitempty"`
 }
@@ -29,13 +30,19 @@ func (dto *List) FromDto(
 	for i, itemDto := range *dto {
 		if itemDto.Id == nil {
 			return nil, fmt.Errorf("item %v has no id", i)
+		} else if itemDto.Kind != nil && !queue.ValidItemKindString(*itemDto.Kind) {
+			return nil, fmt.Errorf("item %v has invalid type: %v", i, *itemDto.Kind)
+		} else if itemDto.Kind != nil && itemDto.Language != nil && *itemDto.Kind == queue.ItemKindNovelString {
+			return nil, fmt.Errorf("item %v has novel type, but provides language", i)
+		} else if itemDto.Language != nil && !work.ValidArtworkLanguageString(*itemDto.Language) {
+			return nil, fmt.Errorf("item %v has invalid language: %v", i, *itemDto.Language)
 		}
 
 		q[i] = queue.Item{
 			Id:       *itemDto.Id,
 			Kind:     utils.FromPtrTransform(itemDto.Kind, queue.ItemKindFromString, defaultKind),
 			Size:     utils.FromPtrTransform(itemDto.Size, imageext.SizeFromUint, defaultSize),
-			Language: defaultLanguage, // FIXME:
+			Language: utils.FromPtrTransform(itemDto.Language, work.LanguageFromString, defaultLanguage),
 			OnlyMeta: utils.FromPtr(itemDto.OnlyMeta, defaultOnlyMeta),
 			Paths:    utils.FromPtr(itemDto.Paths, defaultPaths),
 		}
